31ee16204de9d94d72c984cc00703cb7327cb502
Karol Pietryka <karol.pietryka11@gmail.com>
2023-11-11 17:09:14 +0100

    [Add]Add spring app init code

---
+++ src/test/kotlin/com/kp/futurecriteria/FutureCriteriaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kp.futurecriteria
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FutureCriteriaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
