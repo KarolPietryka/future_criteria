4c105a767856c71b3af84e8743db371be223679c
Karol Pietryka <karol.pietryka11@gmail.com>
2023-11-18 14:19:29 +0100

    [Improve]Improve on flex positioning

--- front/src/component/HomePage.js
+++ front/src/component/HomePage.js
@@ -2,14 +2,14 @@ import React from 'react';
 import SidePanel from "./SidePanel";
 const HomePage = () => {
     return (
-        <div style={{ display: 'flex', height: '100%' }}>
+        <div style={{ display: 'flex', height: '100vh' }}>
             <SidePanel />
-            <div style={{ flex: 1 }}>
+            <div style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
                 <header style={{ backgroundColor: '#f8f9fa', padding: '10px', textAlign: 'center' }}>
                     <h1>Task Management System</h1>
                 </header>
 
-                <main style={{ margin: '20px' }}>
+                <main style={{ margin: '20px', flexGrow: 1 }}>
                     <h2>Projects and Tasks</h2>
                     <p>Project and task details will be displayed here.</p>
                 </main>
